39093f43fadb28f8fd8063eba9554516079b4a25
Kheya <s.m.kheya@gmail.com>
2021-03-21 22:13:46 +0600

    clea

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -128,12 +128,7 @@ The above copyright notice and this permission notice shall be included in all c
                         <p>Notifications</p>
                     </a>
                 </li>
-                <li class="nav-item ">
-                    <a class="nav-link" href="./rtl.html">
-                        <i class="material-icons">language</i>
-                        <p>RTL Support</p>
-                    </a>
-                </li>
+                
                 <li class="nav-item active-pro ">
                     <a class="nav-link" href="./upgrade.html">
                         <i class="material-icons">unarchive</i>
@@ -143,29 +138,7 @@ The above copyright notice and this permission notice shall be included in all c
             </ul>
         </div>
     </div>
-    <div class="main-panel">
-        <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
-            <div class="container-fluid">
-                <div class="navbar-wrapper">
-                    <a class="navbar-brand" href="javascript:;">Dashboard</a>
-                </div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="navbar-toggler-icon icon-bar"></span>
-                    <span class="navbar-toggler-icon icon-bar"></span>
-                    <span class="navbar-toggler-icon icon-bar"></span>
-                </button>
-                <div class="collapse navbar-collapse justify-content-end">
-                    <form class="navbar-form">
-                        <div class="input-group no-border">
-                            <input type="text" value="" class="form-control" placeholder="Search...">
-                            <button type="submit" class="btn btn-white btn-round btn-just-icon">
-                                <i class="material-icons">search</i>
-                                <div class="ripple-container"></div>
-                            </button>
-                        </div>
-                    </form>
+
                     <ul class="navbar-nav">
                         <li class="nav-item">
                             <a class="nav-link" href="javascript:;">
